4260bc2a70b0bbe725abfd7833a7cc8e430106e8
christineKarimi <karimikim3@gmail.com>
2019-09-11 14:55:11 +0300

    Added alert message functions to notify users of actions

--- js/shopify.js
+++ js/shopify.js
@@ -1,15 +1,14 @@
+// alert messages for user actions
 $(function () {
-            var $cartItems = $(".cart-item"),
-                getPrice = /(\d+)\.(\d{2})/g,
-                animationEnd = "webkitAnimationEnd oanimationend oAnimationEnd animationend",
-                removedItemAlert =
-                '<li class="is-added"><div class="alert alert-default" data-mod="undo-alert">{{ Item name }} has been removed from your cart. <a href="" class="btn btn-secondary" data-mod="undo-delete-btn">↩ Undo</a></div></div>',
-                emptyCartAlert =
-                '<li class="is-added"><div class="alert alert-default"><h3 class="alert-header">Bummer, your cart\'s empty.</h3><div class="alert-body"><p>You currently don\'t have any items in your cart. Need some help? Check out our: </p><a href="" class="btn btn-primary">Products</a><span class=""> or </span><a href="" class="btn btn-deals">Specials</a></div></div></li>',
-                removedItems = 0;
-            getSubTotal = function () {
-                var subTotal = itemPrice * itemQty;
-                return subTotal.toFixed(2);
-            };
-
-            
+    var $cartItems = $(".cart-item"),
+        getPrice = /(\d+)\.(\d{2})/g,
+        animationEnd = "webkitAnimationEnd oanimationend oAnimationEnd animationend",
+        removedItemAlert =
+            '<li class="is-added"><div class="alert alert-default" data-mod="undo-alert">{{ Item name }} has been removed from your cart. <a href="" class="btn btn-secondary" data-mod="undo-delete-btn">↩ Undo</a></div></div>',
+        emptyCartAlert =
+            '<li class="is-added"><div class="alert alert-default"><h3 class="alert-header">Bummer, your cart\'s empty.</h3><div class="alert-body"><p>You currently don\'t have any items in your cart. Need some help? Check out our: </p><a href="" class="btn btn-primary">Products</a><span class=""> or </span><a href="" class="btn btn-deals">Specials</a></div></div></li>',
+        removedItems = 0;
+    getSubTotal = function () {
+        var subTotal = itemPrice * itemQty;
+        return subTotal.toFixed(2);
+    };
